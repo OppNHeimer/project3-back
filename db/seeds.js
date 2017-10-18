85b2a37b2e7d259c673c8697b31bad32c7ab7452
aspittel <ali07cat07@gmail.com>
2017-10-18 09:48:12 -0400

    adding feedback

--- db/seeds.js
+++ db/seeds.js
@@ -1,7 +1,9 @@
 const mongoose = require('./connection')
-var Post = mongoose.model('Post')
-var Comment = mongoose.model('Comment')
-var Tag = mongoose.model('Tag')
+const Post = mongoose.model('Post')
+const Comment = mongoose.model('Comment')
+const Tag = mongoose.model('Tag')
+
+// AS: Changed some formatting, otherwise looks good! 
 
 joePost = new Post({
     name: 'joeshmo',
@@ -10,6 +12,7 @@ joePost = new Post({
     tags: ['post', 'fun', 'joe'],
 
 })
+
 moePost = new Post({
     name: 'moeshmo',
     title: 'My Post is better than Joes',
@@ -17,6 +20,7 @@ moePost = new Post({
     tags: ['post', 'better', 'moe'],
 
 })
+
 woahPost = new Post({
     name: 'woahshmo',
     title: 'Chill guys',
@@ -29,46 +33,46 @@ sallyComment = new Comment({
     content: 'I agree with Moe, Joe sucks.',
     post: moePost._id
 })
+
 jayComment = new Comment({
     name: 'Jay',
     content: 'Sally, Joe isnt that bad he only kinda sucks',
     post: woahPost._id
 })
+
 joeComment = new Comment({
     name: 'joeshmo',
     content: 'nah guys moes the worst',
     post: woahPost._id
 })
+
 jayTag = new Tag({
     name: 'Test',
     post: woahPost._id
 })
+
 testTag = new Tag({
     name: 'Dia',
     post: moePost._id
 })
 
-commentSeeds = [sallyComment, jayComment, joeComment]
+const commentSeeds = [sallyComment, jayComment, joeComment]
 
-postSeeds = [joePost, moePost, woahPost]
+const postSeeds = [joePost, moePost, woahPost]
 
-tagSeeds = [jayTag, testTag]
+const tagSeeds = [jayTag, testTag]
 
-Post.remove({})
-.then(() => {
+Post.remove({}).then(() => {
     Post.collection.insert(postSeeds)
 })
 
-Comment.remove({})
-.then(() => {
+Comment.remove({}).then(() => {
     Comment.collection.insert(commentSeeds)
 })
 
-Tag.remove({})
-.then(() => {
-    Tag.collection.insert(tagSeeds)
-    .then(() => {
-            process.exit()
+Tag.remove({}).then(() => {
+    Tag.collection.insert(tagSeeds).then(() => {
+        process.exit()
     })
 })
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+// AS: Make sure to remove unused code!
 const express = require('express')
 const parser = require('body-parser')
 let mongoose = require('./db/connection.js')
-// const mongoose = require('./db/connections.js')
 const cors = require('cors')
 const app = express()
 
@@ -10,99 +10,98 @@ const Comment = mongoose.model('Comment')
 const Tag = mongoose.model('Tag')
 
 app.use(parser.json())
-app.use(cors());
+app.use(cors())
 
+// AS: make sure spacing is consistent throughout!
+// Also, your comments are pretty well handled by your
+// function declarations, so they are unneeded
 
-//create comment
-app.post('/createComment', (req, res) => {
-    Comment.create(req.body)
-        .then((comment) => {
-            res.json(comment)
-        })
+// AS: for UI reasons, I would keep urls kebab case instead
+// of camel case - no shift key needed!
+
+// AS: since your lines are pretty short, I would keep the .then on the same line
+
+// AS: Other than that, this looks 💯
+app.post('/create-comment', (req, res) => {
+    Comment.create(req.body).then((comment) => {
+        res.json(comment)
+    })
 })
 
-//index posts
+
 app.get('/', (req, res) => {
-    Post.find({})
-    .then((posts) => {
+    Post.find({}).then((posts) => {
         res.json(posts)
     })
 })
-// index tags
+
+
 app.get('/tags', (req, res) => {
-    Tag.find({})
-    .then((tags) => {
+    Tag.find({}).then((tags) => {
         res.json(tags)
     })
 })
 
-//create new post
+
 app.post('/postCreate', (req, res) => {
-    Post.create(req.body)
-    .then((post) => {
+    Post.create(req.body).then((post) => {
         res.json(post)
     })
 })
 
 
-//show post
 app.get('/:postId', (req, res) => {
-    Post.findOne({_id: req.params.postId})
-    .then((post) => {
+    Post.findOne({_id: req.params.postId}).then((post) => {
         res.json(post)
     })
 })
 
-//edit post
+
 app.post('/:postId/updatePost', function(req,res){
-  Post.findOneAndUpdate({_id: req.params.postId},req.body,{new: true})
+  Post.findOneAndUpdate({_id: req.params.postId}, req.body, {new: true})
       .then((post) => {
-          res.json(post);
+          res.json(post)
   })
 })
 
-//delete post
+
 app.post('/:postId/deletePost', function(req, res){
   Post.findOneAndRemove({_id: req.params.postId}, function(){
     res.json("/")
   })
 })
 
-//index post's comments
+
 app.get('/:postId/comments', (req, res) => {
-    Comment.find({post: req.params.postId})
-    .then((comments) => {
+    Comment.find({post: req.params.postId}).then((comments) => {
         res.json(comments)
     })
 })
 
-//show single comment
+
 app.get('/:post/comments/:commentId', (req, res) => {
-    Comment.find({_id: req.params.commentId})
-    .then((comment) => {
+    Comment.find({_id: req.params.commentId}).then((comment) => {
         res.json(comment)
     })
 })
 
-//create new tag
+
 app.post('/createtag', (req, res) => {
-    Tag.create(req.body)
-        .then((tag) => {
+    Tag.create(req.body).then((tag) => {
             res.json(tag)
         })
 })
 
-//index post's tags
+
 app.get('/:postId/tags', (req, res) => {
-    Tag.find({post: req.params.postId})
-    .then((tags) => {
+    Tag.find({post: req.params.postId}).then((tags) => {
         res.json(tags)
     })
 })
 
+
 app.get('/tags/:searchTag', (req, res) => {
-  Tag.find({name: req.params.searchTag})
-  .then((tags) => {
+  Tag.find({name: req.params.searchTag}).then((tags) => {
       res.json(tags)
   })
 })
